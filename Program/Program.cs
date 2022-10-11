098bc8af1b983bbb5c0bfbf597e9266f87a952cf
Karina <Karinanifontova06@mail.ru>
2022-10-11 13:29:07 +0300

    Receive_Data

---
+++ Program/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+try
+{
+    Console.Write ("Введите количество элементов массива: ");
+    int num = Convert.ToInt32 (Console.ReadLine ());
+    string [] array = new string [num];
+    for (int i = 0; i < num; i++)
+    {
+        Console.Write ($"Введите {i + 1} элемент массива: ");
+        array [i] = Console.ReadLine ();
+    }
+}
+catch
+{
+   Console.Write ("Неверно введены данные!");
+}
+
+
